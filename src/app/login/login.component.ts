e489c0513c6ed0ac1d655a31ca004ce8bde658f7
Phillip Doise <p.doise@gmail.com>
2016-12-24 12:51:48 -0700

    login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Login } from './login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
 
-  header:string = 'User Log In';
-
-  ngOnInit() {
-    console.log('Login Page');
-  }
+export class LoginComponent {
+  model = new Login('', '');
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
