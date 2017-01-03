d72fecda098a3f96063f7f6aa95e274eaccd980f
Phillip Doise <p.doise@gmail.com>
2017-01-02 18:55:41 -0700

    site header, nav

---
+++ src/app/common/components/user-navigation/user-navigation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-navigation',
+  templateUrl: './user-navigation.component.html',
+  styleUrls: ['./user-navigation.component.scss']
+})
+
+export class UserNavigationComponent {}
